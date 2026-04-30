b9876bbff0c7e45ec9b8381e08d3b4400db3941e
Rafael Araújo <rafaelaraujo004@gmail.com>
2026-04-30 13:44:35 -0300

    fix: incluir firebase-config.js no repositório

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Credenciais do projeto Firebase.
+window.firebaseConfig = {
+  apiKey: "AIzaSyDk3Ejm2QBfIYUnghOAJzIwF024Rm17bz0",
+  authDomain: "xcmg-89032.firebaseapp.com",
+  projectId: "xcmg-89032",
+  storageBucket: "xcmg-89032.firebasestorage.app",
+  messagingSenderId: "488945477819",
+  appId: "1:488945477819:web:f2832cdda456f4170ae19e",
+  measurementId: "G-GPFPZQBZ12"
+};
